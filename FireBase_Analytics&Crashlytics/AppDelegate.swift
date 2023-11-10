fd44110559eedb5324f3c0e96b21ed49b0e328db
yeomsungpil <present7256@gmail.com>
2023-11-10 14:21:28 +0900

    FireBase Remote Push Notifications /  APNS 적용

--- FireBase_Analytics&Crashlytics/AppDelegate.swift
+++ FireBase_Analytics&Crashlytics/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        // 원격 알림 등록 : 푸시든 로컬이든 알람 허용해야 그 이후가 가능!!
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+
+        application.registerForRemoteNotifications()
+        
+        //FireBaseMessaging
+        Messaging.messaging().delegate = self
+
+        
         return true
     }
 
@@ -34,3 +53,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    // 디바이스 토큰
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("Device - token : \(token)")
+    }
+}
+
+extension AppDelegate : MessagingDelegate {
+    // FireBase 맵핑된 토큰에 관련된 정보를 받아 올 수 있음
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+    }
+
+}
